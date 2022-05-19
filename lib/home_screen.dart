9fa2d1d3927e47d68d755823acfbb2404287b467
Nurul Huda <rulhuda313@gmail.com>
2022-05-19 23:25:52 +0700

    update lagi

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,52 +1,68 @@
 // ignore_for_file: deprecated_member_use
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase/login_page.dart';
+import 'package:firebase/sign_in.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+  const HomeScreen({Key? key, required this.email}) : super(key: key);
+  final String email;
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home -  Nurul Huda(2031710061)'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(80),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Signed In as',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              user.email!,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 40),
-            RaisedButton(
-              color: Colors.lightBlue,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40)),
-              highlightElevation: 0,
-              child: const Text(
-                'Sign Out',
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.white, Colors.white],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              const Text(
+                'EMAIL',
                 style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                ),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
               ),
-              onPressed: () => FirebaseAuth.instance.signOut(),
-            ),
-          ],
+              Text(
+                email,
+                style: const TextStyle(
+                    fontSize: 25,
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 40),
+              RaisedButton(
+                onPressed: () {
+                  signOutGoogle();
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) {
+                    return const LoginPage();
+                  }), ModalRoute.withName('/'));
+                },
+                color: Colors.lightBlue,
+                child: const Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    'SIGN OUT',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,96 +1,187 @@
 // ignore_for_file: deprecated_member_use
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase/home_screen.dart';
-import 'package:firebase/login_email.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase/auth.dart';
 import 'package:firebase/sign_in.dart';
 import 'package:firebase/first_screen.dart';
+import 'package:firebase/home_screen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController(text: "");
+  TextEditingController passwordController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const FlutterLogo(size: 150),
-              const SizedBox(height: 50),
-              _signInButton2(),
-              const SizedBox(height: 16),
-              _signInButton(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _signInButton2() {
-    return RaisedButton(
-      color: Colors.lightBlue,
-      onPressed: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) {
-              return StreamBuilder<User?>(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Something went wrong ${snapshot.error}'),
-                    );
-                  } else if (snapshot.hasData) {
-                    return const HomeScreen();
-                  } else {
-                    return const LoginEmail();
-                  }
-                },
-              );
-            },
-          ),
-        );
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      highlightElevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
+      body: ListView(children: [
+        Column(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Text(
-                'Sign in with Email',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+          children: <Widget>[
+            const SizedBox(height: 20),
+            const FlutterLogo(size: 150),
+            const SizedBox(height: 20),
+            const Text(
+              'Haiii,\n Selamat Datang',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
                 ),
+                controller: emailController,
               ),
-            )
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(
+                  left: 30, right: 30, top: 10, bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: passwordController,
+                obscureText: true,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(
+                  left: 30, right: 30, top: 3, bottom: 15),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Confirm Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: passwordController,
+                obscureText: true,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+              child: ElevatedButton(
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  onPressed: () async {
+                    await signIn(emailController.text, passwordController.text)
+                        .then((result) {
+                      if (result == null) {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen(
+                                      email: emailController.text,
+                                    )));
+                      } else {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                            result,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ));
+                      }
+                    });
+                  }),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+              child: ElevatedButton(
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  onPressed: () async {
+                    signUp(emailController.text, passwordController.text)
+                        .then((result) {
+                      if (result == null) {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen(
+                                      email: emailController.text,
+                                    )));
+                      } else {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                            result,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ));
+                      }
+                    });
+                  }),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: const <Widget>[
+                Expanded(
+                  child: Divider(
+                    thickness: 1,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'OR',
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Divider(thickness: 1, color: Colors.black),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            _signInButton(),
           ],
         ),
-      ),
+      ]),
     );
   }
 
   Widget _signInButton() {
     return RaisedButton(
-      color: Colors.lightBlue,
+      color: Colors.blueGrey,
       onPressed: () {
         signInWithGoogle().then((result) {
           if (result != null) {
@@ -112,10 +203,11 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
+            // Image(image: AssetImage("assets/google.png"), height: 35.0),
             Padding(
               padding: EdgeInsets.only(left: 10),
               child: Text(
-                'Sign in with Google',
+                'Sign In With Google',
                 style: TextStyle(
                   fontSize: 20,
                   color: Colors.white,
